a5cdce214634bf0a54b76f377b90181490e2ab50
raemilla <rachael.miller05@gmail.com>
2016-09-23 11:08:40 -0400

    app component with logic for if a parent or child is logged in -- untested

---
+++ app/assets/javascripts/components/app.es6.jsx
@@ -0,0 +1,23 @@
+class App extends React.Component {
+  constructor(){
+    super()
+    this.displayUserComponent = this.displayUserComponent.bind(this)
+  }
+
+  displayUserComponent(){
+    if(this.props["parent?"]){
+      return (<ParentDashboard />) //is return necessary?
+    } else {
+      <ChildDashboard />
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <pre><code>{JSON.stringify(this.props,null,2)}</code></pre>
+        {displayUserComponent()}
+      </div>
+    )
+  }
+}
