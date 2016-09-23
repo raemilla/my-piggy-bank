26c23bd5b36098fb00dfcf0d73510960f17bba87
raemilla <rachael.miller05@gmail.com>
2016-09-23 16:42:18 -0400

    generate username migration and modified seed to reflect

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Sam = Parent.create(name: "Sam", email:"sam@sam.com", password:"123456")
-Sam_Jr = Child.create(name: "Sam Jr.", password: "123456", parent_id: 1, undeposited_funds: 3)
-Samantha = Child.create(name: "Samantha.", password: "123456", parent_id: 1, undeposited_funds: 5)
+Sam = Parent.create(name: "Sam", username: "sam" email:"sam@sam.com", password:"123456")
+Sam_Jr = Child.create(name: "Sam Jr.", username: "boyjr" password: "123456", parent_id: 1, undeposited_funds: 3)
+Samantha = Child.create(name: "Samantha.", username: "girljr" password: "123456", parent_id: 1, undeposited_funds: 5)
 Samantha_Investment = Investment.create(balance: 3, interest_rate: 2, child_id: 2 )
 Sam_Jr_Investment = Investment.create(balance: 3, interest_rate: 2, child_id: 1 )
 Samantha_Spending = Spending.create(balance: 5, child_id: 2)
