4a5496b0fe138566b7fbb7e4de56018fada91aa3
Moin Uddin <email": null>
2016-09-23 14:24:45 -0400

    Create parent, children controllers and sessionhelper

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @parent = Parent.find_by(email: params[:user][:email])
+    @child = Children.find_by(email: params[:user][:email])
+    if @parent && @parent.authenticate(params[:user][:password])
+      session[:parent_id] = @parent.id
+      redirect_to pages_path
+    elsif @child && @child.authenticate(params[:user][:password])
+      session[:child_id] = @child.id
+      redirect_to pages_path
+    else
+      @errors = ['Invalid email/password']
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tests' => 'tests#index'
+
+get '/tests' => 'tests#index'
+
+
+resources :pages, only: [:index]
+resources :parents
+resources :children
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
+get '/logout' => 'sessions#destroy'
+
 end
