0ff981dfeca1c0b5650dd46a87af86b1faf62faf
raemilla <rachael.miller05@gmail.com>
2016-09-24 16:50:22 -0400

    toggle information for individual banks complete

--- app/assets/javascripts/components/manageChildAccount.es6.jsx
+++ app/assets/javascripts/components/manageChildAccount.es6.jsx
@@ -5,6 +5,7 @@ class ManageChildAccount extends React.Component {
       displayBanks: false
     }
     this.toggleDisplay = this.toggleDisplay.bind(this)
+    this.displayBanks = this.displayBanks.bind(this)
   }
 
   toggleDisplay(){
@@ -14,6 +15,14 @@ class ManageChildAccount extends React.Component {
     })
   }
 
+  displayBanks(){
+    return(
+      <ul>
+        {this.props.child.banks.map((bank, idx) => <li key={idx}>{bank.type}, {bank.balance}</li>)}
+      </ul>
+    )
+  }
+
 
   render(){
     return(
@@ -21,7 +30,7 @@ class ManageChildAccount extends React.Component {
       <li onClick={this.toggleDisplay}>
         <strong>{this.props.child.name}</strong> <br/>
         total undeposited funds: {this.props.child.undeposited_funds}
-        {this.state.displayBanks ? <p>yay toggle</p> : null }
+        {this.state.displayBanks ? this.displayBanks() : null }
       </li>
       <br/>
       </div>
