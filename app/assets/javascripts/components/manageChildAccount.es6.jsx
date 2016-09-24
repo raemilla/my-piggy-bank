9551e94fd4ac6fd0e3749b1e3284146c647462fc
raemilla <rachael.miller05@gmail.com>
2016-09-24 16:46:53 -0400

    toggle functionality put in place

--- app/assets/javascripts/components/manageChildAccount.es6.jsx
+++ app/assets/javascripts/components/manageChildAccount.es6.jsx
@@ -1,7 +1,30 @@
 class ManageChildAccount extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      displayBanks: false
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this)
+  }
+
+  toggleDisplay(){
+    let shouldToggle = !this.state.displayBanks
+    this.setState({
+      displayBanks: shouldToggle
+    })
+  }
+
+
   render(){
     return(
-      <li>{this.props.child.name}</li>
+      <div>
+      <li onClick={this.toggleDisplay}>
+        <strong>{this.props.child.name}</strong> <br/>
+        total undeposited funds: {this.props.child.undeposited_funds}
+        {this.state.displayBanks ? <p>yay toggle</p> : null }
+      </li>
+      <br/>
+      </div>
     )
   }
 }
