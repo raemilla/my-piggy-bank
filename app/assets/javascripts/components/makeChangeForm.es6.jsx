029b8120376e34e257dc2c22f519def356bc63c7
raemilla <rachael.miller05@gmail.com>
2016-09-24 19:10:04 -0400

    some edits to child dashboard display

--- app/assets/javascripts/components/makeChangeForm.es6.jsx
+++ app/assets/javascripts/components/makeChangeForm.es6.jsx
@@ -9,37 +9,37 @@ class ChangeMachine extends React.Component {
   }
 
 	ComponentDidUpdate(){
-  		
+
   }
-  
+
   handleSubmit(event){
   	event.preventDefault()
-  	
+
   	var coins = [25, 10, 5]
   	var coinArray = []
   	var remainder = this.refs.amount.value
   	coins.forEach(function(value){
- 
+
   		let numOfCoins = Math.floor(remainder / value)
-  		
+
   		for(var i=0; i < numOfCoins; i++){
     		coinArray.push(value)
 			}
-			remainder = remainder % value 
+			remainder = remainder % value
 
   	}.bind(this))
 
   	this.setState({
         coins: coinArray
       })
-  	
+
   }
 
    loop(){
-   
+
       if(this.state.coins){
 
-      return( 
+      return(
       	<div className="row">
         {
             this.state.coins.map((coin)=>{
@@ -49,25 +49,26 @@ class ChangeMachine extends React.Component {
       </div>
       )
       }
-  
+
   }
 
 
 
 	render(){
-		
+
 		return(
 			<div className="row">
-				<div className="col-md-2">
-					<h3>Make change</h3>
-					<form onSubmit={this.handleSubmit}>
-					
+				<div className="col-md-2"></div>
+				<div className="col-md-8">
+					<h3 className="text-center">Make change</h3>
+					<form className="form-inline text-center" onSubmit={this.handleSubmit}>
+
 						<div className="form-group">
-					    <label>Amount</label>
+					    <label>Amount: </label>
 					    <input type="text" ref="amount" className="form-control" placeholder="enter in an amount"/>
 		  			</div>
 		  			<button type="submit" className="btn btn-primary">Make Change</button>
-		  		
+
 					</form>
 				</div>
 				{this.loop()}
