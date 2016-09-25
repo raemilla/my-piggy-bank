0a7119c5b41c977ffa9abd3697e2110b89be560a
raemilla <rachael.miller05@gmail.com>
2016-09-25 18:14:39 -0400

    transfer money functional without ajax & refactor display banks per child to be a table

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -7,10 +7,20 @@ class BanksController < ApplicationController
 		if bank.save
 			bank.child.undeposited_funds = bank.child.undeposited_funds - params[:value].to_i
 			bank.child.save
-		
+
 			current_user = child_user.as_json(include: {banks: {methods: :type}})
 			render json: current_user
 		end
+	end
+
+	def transfer
+		child = current_user.children.find_by(name: params[:child])
+		amount = params[:amount].to_i
+		bank = child.banks.find_by(type: params[:fromBank])
+		bank.update_attribute("balance", (bank.balance - amount))
+		bank2 = child.banks.find_by(type: params[:toBank])
+		bank2.update_attribute("balance", (bank2.balance + amount))
+		redirect_to root_path
 	end
 
 end
