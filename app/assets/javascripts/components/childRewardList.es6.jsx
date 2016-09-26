3494ad9bd095f7399f296712ae39aa6263407586
raemilla <rachael.miller05@gmail.com>
2016-09-26 16:11:18 -0400

    generate notification when child indicates they want to buy an item

--- app/assets/javascripts/components/childRewardList.es6.jsx
+++ app/assets/javascripts/components/childRewardList.es6.jsx
@@ -1,6 +1,23 @@
 class ChildRewardList extends React.Component {
+  constructor(){
+    super()
+    this.handlePurchase = this.handlePurchase.bind(this)
+  }
+
+  handlePurchase(event) {
+    event.preventDefault();
+    $.ajax({
+      method: 'post',
+      url: '/notifications',
+      data: {
+        text: this.props.child.name + " wants to buy a reward: " + event.target.name
+      }
+    }).done((response) => {
+      console.log("great success")
+    })
+  }
+
   render(){
-    debugger;
     return(
       <table className="table table-sm table-striped">
         <thead className="thead-default">
@@ -16,7 +33,7 @@ class ChildRewardList extends React.Component {
               <tr key={idx}>
                 <th scope="row">{reward.item}</th>
                 <td>{reward.amount}</td>
-                <td><a>buy</a></td>
+                <td><a name={reward.item} onClick={this.handlePurchase}>buy</a></td>
               </tr>
             )
           }
