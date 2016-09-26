cb992f4aa2737551a139f29e72ff3d1d20fcd516
raemilla <rachael.miller05@gmail.com>
2016-09-26 16:01:13 -0400

    toggle rewards list showing

--- app/assets/javascripts/components/childRewardList.es6.jsx
+++ app/assets/javascripts/components/childRewardList.es6.jsx
@@ -1,7 +1,27 @@
 class ChildRewardList extends React.Component {
   render(){
+    debugger;
     return(
-      <p>sup</p>
+      <table className="table table-sm table-striped">
+        <thead className="thead-default">
+          <tr>
+            <th>Reward</th>
+            <th>Cost</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.props.rewards.map((reward, idx) =>
+              <tr key={idx}>
+                <th scope="row">{reward.item}</th>
+                <td>{reward.amount}</td>
+                <td><a>buy</a></td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     )
   }
 }
--- app/assets/javascripts/components/undepositedFunds.es6.jsx
+++ app/assets/javascripts/components/undepositedFunds.es6.jsx
@@ -25,7 +25,7 @@ class UndepositedFunds extends React.Component {
 					</div>
 					<div className="col-md-3">
 						<button onClick={this.toggleRewardDisplay} type="button" className="btn btn-success btn-lg btn-block">Rewards</button>
-						{this.state.displayRewards ? <ChildRewardList /> : null }
+						{this.state.displayRewards ? <ChildRewardList rewards={this.props.current_child.rewards} /> : null }
 					</div>
 				</div>
 
