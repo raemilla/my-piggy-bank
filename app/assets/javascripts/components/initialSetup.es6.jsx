cbf7b6b32296d720af34236f2c5504805b229f9a
raemilla <rachael.miller05@gmail.com>
2016-09-27 16:34:18 -0400

    add some styling to initial setup form

--- app/assets/javascripts/components/initialSetup.es6.jsx
+++ app/assets/javascripts/components/initialSetup.es6.jsx
@@ -21,28 +21,28 @@ class InitialSetup extends React.Component {
 
   displayForm(){
     return(
-      <form onSubmit={this.createChild}  method="post" action="/children">
+      <form id="initial-setup" onSubmit={this.createChild}  method="post" action="/children">
         <div className="form-group">
           <label htmlFor="childName">Name</label>
-          <input type="text" className="form-control" placeholder="Enter your child's name" name="child[name]" />
+          <input id="input-child-name" type="text" className="form-control" placeholder="Enter your child's name" name="child[name]" />
         </div>
-        <div className="form-group">
+        <div id="input-child-username" className="form-group">
           <label htmlFor="childUsername">Username</label>
           <input type="text" className="form-control" placeholder="Set a username for your child" name="child[username]" />
         </div>
-         <div className="form-group">
+         <div id="input-save-for" className="form-group">
           <label htmlFor="childSaveFor">Save For</label>
-          <input type="text" className="form-control" placeholder="Set a saving item for your child" name="child[save_item]" />
+          <input type="text" className="form-control" placeholder="Set an item for your child to redeem from the savings bank" name="child[save_item]" />
         </div>
-        <div className="form-group">
+        <div id="input-child-password" className="form-group">
           <label htmlFor="childPassword">Password</label>
           <input type="password" className="form-control" placeholder="Set a password for your child" name="child[password]" />
         </div>
-        <div className="form-group">
+        <div id="interest-input" className="form-group">
           <label htmlFor="childInterestRate">Interest Rate</label>
           <input type="number" className="form-control" defaultValue="2" name="interest_rate" />
         </div>
-        <button type="submit" className="btn btn-primary">Add!</button>
+        <button id="add-child-button" type="submit" className="btn btn-primary">Add!</button>
       </form>
     )
   }
@@ -65,13 +65,10 @@ class InitialSetup extends React.Component {
 
   render(){
     return(
-      <div className="row">
-        <div className="col-sm-8">
+        <div>
           <h3 className="header">add a child</h3>
           {this.displayForm()}
         </div>
-        <div className="col-sm-2"></div>
-      </div>
     )
   }
 }
