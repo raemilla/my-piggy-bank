c157e58805f76ed4a6e362c4048b2e52745ff028
Legitcoder <U_Moin75@yahoo.com>
2016-09-28 07:29:11 -0400

    Add tweaks to homepage design

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
   render(){
     return(
       <div className="container">
-        <pre><code>{JSON.stringify(this.props,null,2)}</code></pre>
+        
         {this.displayUserComponent()}
       </div>
     )
